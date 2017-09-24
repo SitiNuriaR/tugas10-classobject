e54a28361197d7fe3cdaab96b3cae68ede0e66c1
SitiNuriaR <nuriarachma1106gmail.com>
2017-09-24 14:54:53 +0700

    menambahkan code 1-17 dan update code 2 & 4

---
+++ 10_AB_10510/code7.php
@@ -0,0 +1,16 @@
+<?php
+class smartphone {
+protected static function beli_smartphone(){
+return "Beli smartphone baru";
+}
+}
+class smartphone_dualsim extends smartphone{
+private static function beli_smartphone_dualsim(){
+return "Beli smartphone dual sim baru";
+}
+public static function beli_semua(){
+//lengkapi baris kode berikut ini
+}
+}
+smartphone_dualsim::beli_semua();
+?>
